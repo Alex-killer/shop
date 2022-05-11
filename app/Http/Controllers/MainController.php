c287c501e08cf562f77a6b5d5244bcec96d3bbda
alex-killer <onefrank@bk.ru>
2022-05-11 23:40:04 +0700

    Initial commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        return view('blog.index');
+    }
+
+    public function categories()
+    {
+        $categories = Category::all();
+        return view('blog.categories', compact('categories'));
+    }
+
+    public function category($code)
+    {
+        $category = Category::where('code', $code)->first();
+        return view('blog.category', compact('category'));
+    }
+
+    public function product($product)
+    {
+        return view('blog.product', compact('product'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'MainController@index')->name('index');
+Route::get('/categories', 'MainController@categories')->name('categories');
+Route::get('/category/{category}', 'MainController@category')->name('category');
+Route::get('/product/{product}', 'MainController@product')->name('product');
+
